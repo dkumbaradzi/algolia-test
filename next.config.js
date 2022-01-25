ca2908146145258e0530decbf65d76fdf726ee60
Denis Kumbaradzi <kdenis@gmail.com>
2022-01-25 00:05:20 -0500

    next config + modules setup

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
-const { config } = require('process');
+const { config } = require('process')
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['www.opentable.com']
+    domains: ['www.opentable.com'],
   },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: { and: [/\.[jt]sx?$/] },
-      use: [{ loader: '@svgr/webpack', options: { icon: true, svgo: { plugins: [{ removeViewbox: false }] } } }],
-    });
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: { icon: true, svgo: { plugins: [{ removeViewbox: false }] } },
+        },
+      ],
+    })
 
     return config
   },
