65f291b5fd1464fa057eaf5960782afe6e97ff8c
Denis Kumbaradzi <kdenis@gmail.com>
2022-01-30 18:14:39 -0500

    add persist/clear search state functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,13 +1,59 @@
-import { useEffect, useState } from 'react'
+import { useCallback, FormEvent } from 'react'
+import { useRouter } from 'next/router'
 import { SearchBox } from 'react-instantsearch-dom'
 import styles from '../styles/SearchBar.module.css'
-import getSearchClient from '../helpers/searchClient'
-import { SearchClient } from 'algoliasearch'
 
 const SearchBar = () => {
-  console.log()
+  const router = useRouter()
+  const { params, ...queryParams } = router.query
+  const currentRefinement = queryParams.q ? queryParams.q.toString() : ''
 
-  return <SearchBox className={styles.container} />
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      const form = e.currentTarget
+      const searchInput = form.querySelector('input[type="search"]') as HTMLInputElement
+
+      if (searchInput) {
+        router.push(
+          {
+            pathname: '/search/1',
+            query: {
+              ...queryParams,
+              q: searchInput.value,
+            },
+          },
+          undefined,
+          { shallow: true }
+        )
+      }
+      console.log(e.currentTarget)
+    },
+    [router, queryParams]
+  )
+
+  const onReset = useCallback(() => {
+    const { q, ...otherParams } = queryParams;
+    router.push(
+      {
+        pathname: '/search/1',
+        query: {
+          ...otherParams
+        },
+      },
+      undefined,
+      { shallow: true }
+    )
+  }, [router, queryParams])
+
+  return (
+    <SearchBox
+      defaultRefinement={currentRefinement}
+      onSubmit={onSubmit}
+      onReset={onReset}
+      className={styles.container}
+    />
+  )
 }
 
 export default SearchBar
